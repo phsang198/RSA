1704092148a77026276be76062e1f481bf11c365
SANG PHẠM HỮU <phamhuusang@vietbando.vn>
2024-05-21 10:58:25 +0700

    first

---
+++ RoBot/Form1.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace RoBot
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.show = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // show
+            // 
+            this.show.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.show.Enabled = false;
+            this.show.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.show.ForeColor = System.Drawing.SystemColors.Control;
+            this.show.Location = new System.Drawing.Point(6, 225);
+            this.show.Multiline = true;
+            this.show.Name = "show";
+            this.show.ReadOnly = true;
+            this.show.Size = new System.Drawing.Size(507, 162);
+            this.show.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(487, 214);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 29);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Gen Key";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.GenKey);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(487, 345);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(105, 29);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Decode";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Decode);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(487, 304);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(105, 29);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Encode";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Encode);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(487, 259);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(105, 29);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Open File";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.OpenFile);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(4, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(139, 20);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "File Path/ Input Text";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.Control;
+            this.label4.Location = new System.Drawing.Point(4, 202);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(166, 20);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Encrypted string/ Result";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(463, 601);
+            this.groupBox1.TabIndex = 17;
+            this.groupBox1.TabStop = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox2.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox2.ForeColor = System.Drawing.SystemColors.Control;
+            this.textBox2.Location = new System.Drawing.Point(6, 320);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(451, 256);
+            this.textBox2.TabIndex = 18;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox3.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox3.ForeColor = System.Drawing.SystemColors.Control;
+            this.textBox3.Location = new System.Drawing.Point(6, 35);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(451, 258);
+            this.textBox3.TabIndex = 14;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.Control;
+            this.label5.Location = new System.Drawing.Point(6, 12);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 20);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Private Key";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.SystemColors.Control;
+            this.label6.Location = new System.Drawing.Point(6, 297);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(77, 20);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Public Key";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.textBox1.Location = new System.Drawing.Point(6, 35);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(507, 164);
+            this.textBox1.TabIndex = 19;
+            this.textBox1.Enter += new System.EventHandler(this.InputTextEnter);
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.InputText);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBox4);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.show);
+            this.groupBox2.Location = new System.Drawing.Point(598, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(519, 601);
+            this.groupBox2.TabIndex = 18;
+            this.groupBox2.TabStop = false;
+            // 
+            // textBox4
+            // 
+            this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox4.Enabled = false;
+            this.textBox4.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox4.ForeColor = System.Drawing.SystemColors.Control;
+            this.textBox4.Location = new System.Drawing.Point(6, 413);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(507, 163);
+            this.textBox4.TabIndex = 21;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(4, 390);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(165, 20);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "Decrypted string/Result";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(487, 383);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(105, 29);
+            this.button5.TabIndex = 19;
+            this.button5.Text = "Reset";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Reset);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1129, 600);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.TransparencyKey = System.Drawing.Color.White;
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private TextBox show;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Label label2;
+        private Label label4;
+        private GroupBox groupBox1;
+        private TextBox textBox3;
+        private Label label5;
+        private Label label6;
+        private GroupBox groupBox2;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private TextBox textBox4;
+        private Label label1;
+        private Button button5;
+    }
+}
---
+++ RoBot/Form1.cs
@@ -0,0 +1,372 @@
+﻿using System; 
+using System.Collections;
+using System.Security.Cryptography;
+using System.Text;
+using System.IO;
+using Microsoft.Win32;
+using System.Windows.Forms;
+using System.IO.Compression;
+using System.Xml.Serialization;
+
+namespace RoBot
+{
+    public partial class Form1 : Form
+    {
+        private bool dragging = false;
+        private Point startPoint = new Point(0, 0);
+        public RSACryptoServiceProvider rsa = new RSACryptoServiceProvider(2048);
+
+        public string path = "";
+        public string inputText = "";
+        public string encryptedText = "";
+        public string decryptedText = "";
+        public string extension = "";
+        public string encryptedFilePath = "encrypted";
+        public string decryptedFilePath = "decrypted";
+        public RSAParameters publicKey ;
+        public RSAParameters privateKey ;
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Bitmap bitmap0 = new Bitmap(pictureBox.Image);
+            //var bt = MakeTransparent(bitmap0, Color.White, 25);
+            //// var bt = MakeTransparent(bitmap0, Color.White, 5);
+            //pictureBox.Image = bt;
+
+            //Bitmap bitmap1 = new Bitmap(pictureBox1.Image);
+            //var bt1 = MakeTransparent(bitmap1, Color.White, 25);
+            //// var bt = MakeTransparent(bitmap0, Color.White, 5);
+            //pictureBox1.Image = bt1;
+
+            //this.BackColor = Color.White;
+            //this.TransparencyKey = Color.White;
+        }
+
+        private void Transparent_Form_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Green;
+            this.TransparencyKey = Color.Green;
+        }
+
+        private void Transparent_images_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private Bitmap MakeTransparent(Bitmap bitmap, Color color, int tolerance)
+        {
+            Bitmap transparentImage = new Bitmap(bitmap);
+
+            for (int i = transparentImage.Size.Width - 1; i >= 0; i--)
+            {
+                for (int j = transparentImage.Size.Height - 1; j >= 0; j--)
+                {
+                    var currentColor = transparentImage.GetPixel(i, j);
+                    if (Math.Abs(color.R - currentColor.R) < tolerance &&
+                      Math.Abs(color.G - currentColor.G) < tolerance &&
+                      Math.Abs(color.B - currentColor.B) < tolerance)
+                        transparentImage.SetPixel(i, j, color);
+                }
+            }
+
+            transparentImage.MakeTransparent(color);
+
+            return transparentImage;
+        }
+
+
+        private void mix(object sender, MouseEventArgs e)
+        {
+            if (dragging)
+            {
+                Point p = PointToScreen(e.Location);
+                this.Location = new Point(p.X - this.startPoint.X, p.Y - this.startPoint.Y);
+            }
+        }
+
+        private void pictureBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            dragging = true;
+            startPoint = new Point(e.X, e.Y);
+        }
+
+        private void picturaBox_MouseUp(object sender, MouseEventArgs e)
+        {
+            dragging = false;
+
+        }
+
+        private void dauX_click(object sender, MouseEventArgs e)
+        {
+            this.Close();
+
+        }
+
+        private void Nhap_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void check_enter(object sender, KeyEventArgs e)
+        {
+        }
+
+        private void OpenFile(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Word files (*.docx)|*.docx|Text files (*.txt)|*.txt|All files (*.*)|*.*";
+
+            // Hiển thị hộp thoại Open File và lấy kết quả trả về
+            openFileDialog.ShowDialog();
+
+            //if (result == true)
+            {
+                show.Clear();
+                textBox1.Clear();
+                textBox4.Clear();
+                // Lấy đường dẫn đến file được chọn
+                path = openFileDialog.FileName;
+                string directory = Path.GetDirectoryName(path);
+                extension = Path.GetExtension(path);
+                encryptedFilePath = directory +"\\"+ encryptedFilePath + extension; 
+                decryptedFilePath = directory +"\\"+ decryptedFilePath + extension; 
+                textBox1.AppendText(path);
+                // Mở file bằng cách sử dụng StreamReader
+                //StreamReader reader = new StreamReader(filePath);
+                // ...
+               
+
+            }
+
+        }
+
+        public string getPublicKey()
+        {
+            var sw = new StringWriter();
+            var xs = new XmlSerializer(typeof(RSAParameters));
+            xs.Serialize(sw, publicKey);
+            return sw.ToString(); 
+        }
+        public string getPrivateKey()
+        {
+            var sw = new StringWriter();
+            var xs = new XmlSerializer(typeof(RSAParameters));
+            xs.Serialize(sw, privateKey);
+            return sw.ToString();
+        }
+        private void GenKey(object sender, EventArgs e)
+        {
+            textBox2.Clear();
+            textBox3.Clear(); 
+            {
+                rsa = new RSACryptoServiceProvider(2048);
+                // Lấy khóa công khai và khóa bí mật
+                publicKey = rsa.ExportParameters(false);
+                privateKey = rsa.ExportParameters(true);
+
+                
+                textBox3.AppendText(getPublicKey()); 
+                textBox2.AppendText(getPrivateKey());
+
+                show.Clear();
+            }
+        }
+        public string Encrypt(string text )
+        {
+            rsa = new RSACryptoServiceProvider(2048);
+            rsa.ImportParameters(publicKey);
+            var data = UTF8Encoding.Unicode.GetBytes(text);
+            var cypher = rsa.Encrypt(data, false);
+            return Convert.ToBase64String(cypher); 
+        }
+        public string Decrypt(string text)
+        {
+            var databytes = Convert.FromBase64String(text);
+            rsa.ImportParameters(privateKey);
+            var res = rsa.Decrypt(databytes, false);
+            return UTF8Encoding.Unicode.GetString(res);
+        }
+        private void Encode(object sender, EventArgs e)
+        {
+
+            show.Clear();
+            textBox4.Clear();
+            string inputText = textBox1.Text;
+            if (inputText == "")
+            {
+                show.AppendText("Vui lòng mở file hoặc nhập text");
+                return;
+            }
+            if (inputText != path)
+            {
+                show.AppendText(Encrypt(inputText));
+                return;
+            }
+
+            if (rsa != null)
+            {
+                byte[] data = File.ReadAllBytes(path);
+
+                if (data == null)
+                {
+                    show.AppendText("File rỗng");
+                }
+
+                if (extension == ".txt")
+                {
+                    // Mã hóa file bằng khóa công khai
+                    byte[] encryptedData = rsa.Encrypt(data, false);
+
+                    // Ghi file kết quả ra đĩa
+                    File.WriteAllBytes(encryptedFilePath, encryptedData);
+                }
+                else
+                {
+                    {
+                        // Open the output file
+                        using (var output = File.Create(encryptedFilePath))
+                        {
+                            //Process the file byte by byte
+                            foreach (byte b in data)
+                            {
+                                //Encrypt the byte
+                                byte[] encrypted = rsa.Encrypt(new[] { b }, false);
+
+                                //Write the encrypted byte to the output file
+                                output.Write(encrypted, 0, encrypted.Length);
+                            }
+                        }
+                    }
+                    //string text = UTF8Encoding.Unicode.GetString(data);
+
+                    //string tmp = Encrypt(UTF8Encoding.Unicode.GetString(data));
+                    //byte[] encryptedData = rsa.Encrypt(data, false);
+                    //byte[] res = Encoding.Unicode.GetBytes(Encrypt(UTF8Encoding.Unicode.GetString(data)));
+                    //File.WriteAllBytes(encryptedFilePath, res);
+                }
+                show.AppendText("Đã mã hóa thành công , mở file để xem");
+            }
+            else
+            {
+                show.AppendText("Vui lòng gen key");
+            }
+        }
+
+        private void Decode(object sender, EventArgs e)
+        {
+            textBox4.Clear();
+            string encryptedText = show.Text;
+            if (encryptedText == "")
+            {
+                show.AppendText("Vui lòng mở file hoặc nhập text");
+                return; 
+            }
+            if (encryptedText != path && encryptedText != "Đã mã hóa thành công , mở file để xem")
+            {
+                textBox4.AppendText(Decrypt(encryptedText));
+                return;
+            }
+            if (rsa != null)
+            {
+                byte[] encryptedData = File.ReadAllBytes(encryptedFilePath);
+
+                if (encryptedData == null)
+                {
+                    show.AppendText("File rỗng");
+
+                }
+                if (extension == ".txt")
+                {
+                    // Giải mã file bằng khóa bí mật
+                    byte[] decryptedData = rsa.Decrypt(encryptedData, false);
+                    //?????????
+                    // Ghi file kết quả ra đĩa
+                    File.WriteAllBytes(decryptedFilePath, decryptedData);
+                }
+                else
+                {
+                    //byte[] decryptedData = rsa.Decrypt(encryptedData, false);
+                    //File.WriteAllBytes(decryptedFilePath, decryptedData);
+
+                    using (var output = File.Create(decryptedFilePath))
+                    {
+                        // Process the file byte by byte
+                        int count = -1;
+                        byte[] arr = new byte[256]; 
+                        foreach (byte b in encryptedData)
+                        {
+                            count++;
+                            arr[count] = b;
+                            // Decrypt the byte
+                            if ( count == 255 )
+                            {
+                                byte[] decrypted = rsa.Decrypt(arr, false);
+
+                                // Write the decrypted byte to the output file
+                                output.Write(decrypted, 0, decrypted.Length);
+                                count = -1; 
+                            }
+                        }
+                    }
+                } 
+                    
+                textBox4.AppendText("Đã giải mã thành công , mở file để xem");
+
+            }
+            else
+            {
+                show.AppendText("Vui lòng gen key");
+
+               // MessageBox.Show("", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            
+        }
+
+        private void InputTextEnter(object sender, EventArgs e)
+        {
+            //show.Clear();
+            //textBox4.Clear();
+            //inputText = textBox1.Text;
+            
+        }
+
+        private void InputText(object sender, KeyEventArgs e)
+        {
+            //if (e.KeyCode == Keys.Enter)
+            //{
+            //    show.Clear();
+            //    textBox4.Clear();
+            //    inputText = textBox1.Text;
+            //}
+            //if (e.KeyCode == Keys.Delete)
+            //{
+            //    show.Clear();
+            //    textBox4.Clear();
+            //    textBox1.Clear(); 
+            //}
+        }
+
+        private void Reset(object sender, EventArgs e)
+        {
+            show.Clear();
+            textBox1.Clear(); 
+            textBox2.Clear(); 
+            textBox3.Clear(); 
+            textBox4.Clear();
+
+            path = "";
+            inputText = "";
+            encryptedText = "";
+            decryptedText = "";
+            extension = "";
+            encryptedFilePath = "encrypted";
+            decryptedFilePath = "decrypted";
+    }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
